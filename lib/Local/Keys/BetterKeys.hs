9902c7bf54a3e1fec5c2e7cab13d402188d7c5e4
drfailer <drfailer.42@gmail.com>
2022-05-31 11:17:35 +0200

    new mapping + clear unused files

--- lib/Local/Keys/BetterKeys.hs
+++ lib/Local/Keys/BetterKeys.hs
@@ -36,7 +36,6 @@ import Local.General.Settings
 import Local.General.Utils
 import Local.Prompt.PromptConfig
 import Local.Prompt.CustomPrompts
-import Local.Prompt.CalcPrompt
 
 
 --------------------------------------------------------------------------------
@@ -53,8 +52,7 @@ myAdditionalKeys =
     , ("M-f", searchPrompt myXPConfig)
     , ("M-S-p s", soundPrompt myXPConfig)
     , ("M-S-p f", configPrompt myXPConfig)
-    , ("M-S-p c", calcPrompt myXPConfig)
-    -- , ("M-S-p b", brightnessPrompt myXPConfig) -- (laptop config)
+    , ("M-S-p b", brightnessPrompt myXPConfig) -- (laptop config)
 
     , ("M-S-q", io (exitWith ExitSuccess))
     , ("M-q", spawn "xmonad --recompile; xmonad --restart")
@@ -64,7 +62,8 @@ myAdditionalKeys =
     , ("M-C-<Return>", spawn "st -t FloatTerm")
 
     -- Window navigation
-    , ("M-<Tab>", windows W.focusDown)
+    , ("M-n", windows W.focusDown)
+    , ("M-S-n", windows W.focusUp)
     , ("M-h", sendMessage $ Go L)
     , ("M-j", sendMessage $ Go D)
     , ("M-k", sendMessage $ Go U)
@@ -76,55 +75,74 @@ myAdditionalKeys =
     -- Moving windows
     , ("M-S-w h", sendMessage (IncMasterN 1))
     , ("M-S-w l", sendMessage (IncMasterN (-1)))
-    , ("M-S-h", sendMessage $ Swap L)
-    , ("M-S-j", sendMessage $ Swap D)
-    , ("M-S-k", sendMessage $ Swap U)
-    , ("M-S-l", sendMessage $ Swap R)
+    , ("M-w h", sendMessage $ Swap L)
+    , ("M-w j", sendMessage $ Swap D)
+    , ("M-w k", sendMessage $ Swap U)
+    , ("M-w l", sendMessage $ Swap R)
 
     -- resizing
-    , ("M-M1-h", sendMessage Shrink)
-    , ("M-M1-l", sendMessage Expand)
-    , ("M-M1-j", sendMessage MirrorShrink)
-    , ("M-M1-k", sendMessage MirrorExpand)
+    , ("M-M1-S-h", sendMessage Shrink)
+    , ("M-M1-S-l", sendMessage Expand)
+    , ("M-M1-S-j", sendMessage MirrorShrink)
+    , ("M-M1-S-k", sendMessage MirrorExpand)
 
     -- tabs manipulation
-    , ("M-S-o h", sendMessage $ pullGroup L)
-    , ("M-S-o l", sendMessage $ pullGroup R)
-    , ("M-S-o k", sendMessage $ pullGroup U)
-    , ("M-S-o j", sendMessage $ pullGroup D)
-    , ("M-S-o m", withFocused (sendMessage . MergeAll))
-    , ("M-S-o u", withFocused (sendMessage . UnMergeAll))
-    , ("M-M1-;", onGroup W.focusUp')
-    , ("M-M1-,", onGroup W.focusDown')
+    , ("M-<Tab> h", sendMessage $ pullGroup L)
+    , ("M-<Tab> l", sendMessage $ pullGroup R)
+    , ("M-<Tab> k", sendMessage $ pullGroup U)
+    , ("M-<Tab> j", sendMessage $ pullGroup D)
+    , ("M-<Tab> m", withFocused (sendMessage . MergeAll))
+    , ("M-<Tab> u", withFocused (sendMessage . UnMergeAll))
+    , ("M-;", onGroup W.focusUp')
+    , ("M-,", onGroup W.focusDown')
 
     -- Resize floating windows
-    , ("M-S-C-l", withFocused (keysResizeWindow (50, 0) (0, 1)))
-    , ("M-S-C-h", withFocused (keysResizeWindow (-50, -0) (0, 1)))
-    , ("M-S-C-k", withFocused (keysResizeWindow (0, -50) (0, 0)))
-    , ("M-S-C-j", withFocused (keysResizeWindow (0, 50) (0, 0)))
+    , ("M-S-<F1>", withFocused (keysResizeWindow (50, 0) (0, 1)))
+    , ("M-S-<F2>", withFocused (keysResizeWindow (0, 50) (0, 0)))
+    , ("M-S-<F3>", withFocused (keysResizeWindow (-50, -0) (0, 1)))
+    , ("M-S-<F4>", withFocused (keysResizeWindow (0, -50) (0, 0)))
 
       -- Move floating windows:
-    , ("M-C-h", withFocused (keysMoveWindow (-50, 0)))
-    , ("M-C-j", withFocused (keysMoveWindow (0, 50)))
-    , ("M-C-k", withFocused (keysMoveWindow (0, -50)))
-    , ("M-C-l", withFocused (keysMoveWindow (50, 0)))
+    , ("M-C-S-h", withFocused (keysMoveWindow (-50, 0)))
+    , ("M-C-S-j", withFocused (keysMoveWindow (0, 50)))
+    , ("M-C-S-k", withFocused (keysMoveWindow (0, -50)))
+    , ("M-C-S-l", withFocused (keysMoveWindow (50, 0)))
 
     -- app launch
     , ("M-S-a m", spawn $ termLaunch ++ "neomutt")
     , ("M-S-a f", spawn "brave")
+    , ("M-S-a e", spawn "emacs")
+    , ("M-S-a o", spawn "emacsclient -c")
 
     -- dmenu scripts
     -- , ("M-p", spawn "dmenu_run -p 'Run:'")
     -- , ("M-f", spawn "exec ~/.scripts/dmenu/search/search")
     -- , ("M-S-p s", spawn "exec ~/.scripts/dmenu/sound/sound")
     -- , ("M-S-p c", spawn "exec ~/.scripts/dmenu/editconf/editconf")
-    , ("M-n r", spawn "exec ~/.scripts/dmenu/notes/read-note")
-    , ("M-n e", spawn "exec ~/.scripts/dmenu/notes/edit-notes")
+    -- , ("M-n r", spawn "exec ~/.scripts/dmenu/notes/read-note")
+    -- , ("M-n e", spawn "exec ~/.scripts/dmenu/notes/edit-notes")
 
     -- Spacing: windows and borders
-    , ("M-a", (incWindowSpacing 10))
-    , ("M-S-a", (decWindowSpacing 10))
-    , ("M-z", (incScreenSpacing 10))
-    , ("M-S-z", (decScreenSpacing 10))
+    , ("M-<F11>", (incWindowSpacing 10))
+    , ("M-S-<F11>", (decWindowSpacing 10))
+    , ("M-<F12>", (incScreenSpacing 10))
+    , ("M-S-<F12>", (decScreenSpacing 10))
     , ("M-S-b", (toggleScreenSpacingEnabled >> toggleWindowSpacingEnabled))
+
+    -- Remap keys
+    , ("M-S-h", spawn "sleep 0.1 && xdotool key --clearmodifiers Left")
+    , ("M-S-j", spawn "sleep 0.1 && xdotool key --clearmodifiers Down")
+    , ("M-S-k", spawn "sleep 0.1 && xdotool key --clearmodifiers Up")
+    , ("M-S-l", spawn "sleep 0.1 && xdotool key --clearmodifiers Right")
+    , ("M-C-h", spawn "xdotool mousemove_relative -- -100 0")
+    , ("M-C-j", spawn "xdotool mousemove_relative 0 100")
+    , ("M-C-k", spawn "xdotool mousemove_relative 0 -100")
+    , ("M-C-l", spawn "xdotool mousemove_relative 100 0")
+    , ("M-C-M1-h", spawn "xdotool mousemove_relative -- -10 0")
+    , ("M-C-M1-j", spawn "xdotool mousemove_relative 0 10")
+    , ("M-C-M1-k", spawn "xdotool mousemove_relative 0 -10")
+    , ("M-C-M1-l", spawn "xdotool mousemove_relative 10 0")
+    , ("M-C-u", spawn "xdotool click --clearmodifiers 1")
+    , ("M-C-i", spawn "xdotool click --clearmodifiers 2")
+    , ("M-C-o", spawn "xdotool click --clearmodifiers 3")
     ]
